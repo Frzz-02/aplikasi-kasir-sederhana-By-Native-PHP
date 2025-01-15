13d6152471f05e6c1e281ed066a54801e5fccf61
Frzz-02 <pc.feriirawan0211@gmail.com>
2025-01-15 22:36:51 +0700

    mengubah rute pengiriman data langsung ke dashboard

--- interface/register.php
+++ interface/register.php
@@ -11,7 +11,7 @@ if (isset($_POST['submit'])) {
             echo "
                 <script>
                 alert('" . $kondisi[1] . "');
-                document.location.href='login.php';
+                document.location.href='../dashboard.php';
                 </script>
             ";
             
