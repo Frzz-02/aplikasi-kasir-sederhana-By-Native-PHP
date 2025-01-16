f52c64f73db3893f63a115c426b8b618e41c3f49
Frzz-02 <pc.feriirawan0211@gmail.com>
2025-01-16 10:28:38 +0700

    menambah folder yang berisi file bagian yang lebih umum

---
+++ main-page/general/sidebar.php
@@ -0,0 +1,16 @@
+    <nav class="sidebar">
+        <div class="logo-container">
+            <img src="../../assets/logo.png" alt="World Donuts Logo" class="logo">
+            <span class="logo-text">ADMIN DASHBOARD</span>
+        </div>
+        <ul class="nav-menu">
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="../interface/register.php">Register Pengguna</a></li>
+            <li><a href="#">Transaksi</a></li>
+            <li><a href="manajemenB.php">Manajemen Barang</a></li>
+            <li><a href="manajemenP.php">Manajemen Pelanggan</a></li>
+            <li><a href="#">Invoice</a></li>
+            <li><a href="#">Laporan</a></li>
+            <li><a href="../function/logout.php">Logout</a></li>
+        </ul>
+    </nav>
