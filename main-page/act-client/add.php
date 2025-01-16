e125c7a76794f5732f095d10620d0278243bd3ee
Frzz-02 <pc.feriirawan0211@gmail.com>
2025-01-16 22:38:14 +0700

    menambah folder act-client utk memisahkan crud form

---
+++ main-page/act-client/add.php
@@ -0,0 +1,34 @@
+<div class="modal fade" id="tambahPelanggan" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Tambah Pelanggan Baru</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="post" action="">
+                        <div class="mb-3">
+                            <label class="form-label">Nama Pelanggan</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">No. Telepon</label>
+                            <input type="tel" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Alamat</label>
+                            <textarea class="form-control"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Email</label>
+                            <input type="email" class="form-control">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                    <button type="button" class="btn btn-primary">Simpan</button>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ main-page/act-client/update.php
@@ -0,0 +1,29 @@
+<div class="modal fade" id="editPelanggan" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Edit Data Pelanggan</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="post" action="">
+                        <div class="mb-3">
+                            <label class="form-label">Nama Pelanggan</label>
+                            <input name="username" type="text" class="form-control" value="John Doe">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Gender</label>
+                            <input type="tel" class="form-control" value="081234567890">
+                        </div>
+                        
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                        <button type="button" class="btn btn-primary">Simpan Perubahan</button>
+                    </div>
+                </form>
+
+                
+            </div>
+        </div>
+    </div>
