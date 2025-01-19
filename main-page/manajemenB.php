a81580295e287b65006117a90337594f726154dd
Frzz-02 <pc.feriirawan0211@gmail.com>
2025-01-19 18:04:58 +0700

    meng-include kan sidebar yang di buat di file tersendiri

--- main-page/manajemenB.php
+++ main-page/manajemenB.php
@@ -6,6 +6,20 @@
     <title>Halaman Barang</title>
     <link rel="stylesheet" href="../css/dashboard.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    
+
+
+</head>
+<body>
+
+<?php
+    require 'general/sidebar.php';
+?>
+
+
+
+
     <style>
 .destination-card img {
     width: 100%;
@@ -18,26 +32,6 @@
     padding: 10px;
 }
 </style>
-
-</head>
-<body>
-
-<nav class="sidebar">
-        <div class="logo-container">
-            <img src="assets/logo.png" alt="World Donuts Logo" class="logo">
-            <span class="logo-text">ADMIN DASHBOARD</span>
-        </div>
-        <ul class="nav-menu">
-        <li><a href="dashboard.php">Dashboard</a></li>
-            <li><a href="interface/register.php">Register Pengguna</a></li>
-            <li><a href="#">Transaksi</a></li>
-            <li><a href="manajemenB.php">Manajemen Barang</a></li>
-            <li><a href="manajemenP.php">Manajemen Pelanggan</a></li>
-            <li><a href="#">Invoice</a></li>
-            <li><a href="#">Laporan</a></li>
-            <li><a href="function/logout.php">Logout</a></li>
-        </ul>
-    </nav>
     
     <div class="container my-4">
     <header class="text-center mb-4">
