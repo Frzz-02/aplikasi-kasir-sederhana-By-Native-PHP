2c1c3a4f974a8d91a1610d63107716a2aab96420
Frzz-02 <pc.feriirawan0211@gmail.com>
2025-01-19 17:38:49 +0700

    memperbaiki jika terdapat koneksi error

--- function/koneksi.php
+++ function/koneksi.php
@@ -6,8 +6,7 @@ $database = "toko";
 
 $koneksi = mysqli_connect($hostname, $username, $password, $database);
 if ($koneksi->connect_error) {
-    echo "koneksi database rusak ";
-    die("error: " . $koneksi->connect_error);
+    return die("error: " . $koneksi->connect_error);
 }
 
 
