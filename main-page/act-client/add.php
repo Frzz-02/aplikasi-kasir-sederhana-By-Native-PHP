9e403b1835cf308e93ea21e3f7cab7e69b4b9794
Frzz-02 <pc.feriirawan0211@gmail.com>
2025-01-19 17:52:32 +0700

    menambahkan fitur generate token
    ini untuk mengatasi jika pengguna mereload halaman dan hal itu akan memunculkan pop up dan ketika memencet ok maka data sebelumnya akan otomatis ditambahkan (alias akan mengalami bug ketika di reload)

--- main-page/act-client/add.php
+++ main-page/act-client/add.php
@@ -1,3 +1,12 @@
+<?php
+$_SESSION['token'] = bin2hex(random_bytes(32));
+
+?>
+
+
+
+
+
 <div class="modal fade" id="tambahPelanggan" tabindex="-1">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -7,27 +16,26 @@
                 </div>
                 <div class="modal-body">
                     <form method="post" action="">
+
+                    <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>">
                         <div class="mb-3">
                             <label class="form-label">Nama Pelanggan</label>
-                            <input type="text" class="form-control">
+                            <input name="nama" type="text" class="form-control">
                         </div>
+
+
                         <div class="mb-3">
-                            <label class="form-label">No. Telepon</label>
-                            <input type="tel" class="form-control">
+                            <label class="form-label">Gender</label>
+                            <input name="gender" type="tel" class="form-control">
                         </div>
-                        <div class="mb-3">
-                            <label class="form-label">Alamat</label>
-                            <textarea class="form-control"></textarea>
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Email</label>
-                            <input type="email" class="form-control">
+
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                            <button name="submit" type="submit" value="tambah" class="btn btn-primary">Simpan</button>
                         </div>
                     </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                    <button type="button" class="btn btn-primary">Simpan</button>
+                
                 </div>
             </div>
         </div>
