5923bf8f53900ef074ed63b296c4b0187bf84d26
Frzz-02 <pc.feriirawan0211@gmail.com>
2025-02-25 19:33:17 +0700

    menambah icon dan memperbaiki bug

--- main-page/act-transaction/receipt.php
+++ main-page/act-transaction/receipt.php
@@ -4,8 +4,8 @@
 
 
 
-    $token = '121212';//generate_ID_Transaction();
-    // insert_data_transaksi($_POST, $token);
+    $token = generate_ID_Transaction();
+    insert_data_transaksi($_POST, $token);
 
 
 // var_dump($_POST);die;
@@ -104,7 +104,9 @@
         <div class="invoice-header">
             <h1>Invoice Pembayaran</h1>
             <p class="text-muted">Transaksi Berhasil</p>
-            <button class="print-button" onclick="window.print()">==</button>
+                <button class="print-button" onclick="window.print()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-printer"><path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2"/><path d="M6 9V3a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v6"/><rect x="6" y="14" width="12" height="8" rx="1"/></svg>
+                </button>
 
     <head>
         <meta charset="UTF-8">
--- main-page/invoice.php
+++ main-page/invoice.php
@@ -52,7 +52,7 @@ $harga_barang = [];
     $id_pelanggan = $data_transaksi[0]['id_pelanggan'];
 
 
-var_dump($data_transaksi);
+// var_dump($data_transaksi);
 
 
 ?>
@@ -141,7 +141,9 @@ var_dump($data_transaksi);
         <div class="invoice-header">
             <h1>Invoice Pembayaran</h1>
             <p class="text-muted">Transaksi Berhasil</p>
-            <button class="print-button" onclick="window.print()">==</button>
+                <button class="print-button" onclick="window.print()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-printer"><path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2"/><path d="M6 9V3a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v6"/><rect x="6" y="14" width="12" height="8" rx="1"/></svg>
+                </button>
 
     <head>
         <meta charset="UTF-8">
@@ -188,7 +190,6 @@ var_dump($data_transaksi);
                                 
                                 
 
-                           
                             </tr>
                             
                             <?php endfor; ?>
